20588b5657caafd35a5c2fa16854d6c87b6098ad
Jumabek <Jumabek4044@gmail.com>
2022-08-28 03:19:05 +0000

    rm zip file

--- data/get_kemophone.sh
+++ data/get_kemophone.sh
@@ -2,11 +2,12 @@
 
 FILE=aggregated_csv.zip
 if [ -f "$FILE" ]; then
-    echo "$FILE exists."
+    echo "$FILE exists. Please remove the zip file to start fresh"
 else 
     echo "$FILE does not exist. Downloadiong"
     gdown --id 1rH6h9fpZZG6A7eXDN5MZ7u_A3lAjiAaY 
+    unzip $FILE -d kemophone
+    rm $FILE
 fi
 
 
-unzip $FILE -d kemophone
